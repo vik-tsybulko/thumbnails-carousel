b85868f17f17559537f5ee3b08d212113f9716b4
Vik Tsybulko <vik.tsybulko@gmail.com>
2020-11-06 16:43:02 +0200

    Thumbnail component was implemented

---
+++ src/app/thumbnail/thumbnail.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-thumbnail',
+  templateUrl: './thumbnail.component.html',
+  styleUrls: ['./thumbnail.component.scss']
+})
+export class ThumbnailComponent {
+  @Input() thumbnail: any;
+  @Input() tabIndex: number;
+  @Input() active: boolean;
+
+  constructor() { }
+
+}
